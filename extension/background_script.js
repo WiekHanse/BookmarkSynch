78ccb91f46ecedb99f11c05cb77ccc38c102cc1c
Henri Wiek <henri.wiek@hansemerkur.de>
2024-09-18 14:34:37 +0200

    json hinzugefügt

--- extension/background_script.js
+++ extension/background_script.js
@@ -1,20 +1,15 @@
-let getBookmark = browser.bookmarks.get("--nhkV63O7Rp");
-const URL = 'https://dummy.restapiexample.com/employee'
-
-console.log(getBookmark);
-
+const TIGERA_DEV_URL = 'https://gitpro.hanse-merkur.de/WIEKH/bookmarks/-/raw/main/bookmarks/tigera_dev.json'
 browser.bookmarks.update(
     "--nhkV63O7Rp",
     {
-        title: "BG2",
-        url: "https://www.youtube.com/watch?v=pekvDs_h-B8"
+        title: "test",
+        url: "https://www.youtube.com"
     }
 )
 
-fetch(URL)
+fetch(TIGERA_DEV_URL)
     .then(response => response.json())
     .then(data => console.log(data))
     .catch(err => console.log(err))
-
 
 //todo --> ID der Bookmarks + Speicherort und aenderungen per const einpflegen
