70a90d1e2d7ad0ac9fcc5d1b9240c7842d6018c2
Henri Wiek <henri.wiek@hansemerkur.de>
2024-09-25 10:04:41 +0200

    feat: Speicherfeedback und reload hinzugefuegt

--- extension/options.js
+++ extension/options.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const form = document.getElementById('bookmarkForm');
+    const messageDiv = document.getElementById('message');
     form.addEventListener('submit', function (event) {
         event.preventDefault();
         const ordnertitle = document.getElementById('ordnertitle').value;
@@ -8,11 +9,23 @@ document.addEventListener('DOMContentLoaded', function () {
         if (url) {
             console.log('URL:', url);
             localStorage.setItem('url', url);
+            document.getElementById('url').value = "";
         }
 
         if (ordnertitle) {
             console.log('Ordnertitel:', ordnertitle);
             localStorage.setItem('ordnerTitel', ordnertitle);
+            document.getElementById('ordnertitle').value = "";
         }
+        messageDiv.textContent = 'Daten wurden erfolgreich übernommen!';
+        messageDiv.style.display = 'block';
+
+        setTimeout(function () {
+            messageDiv.style.display = 'none';
+        }, 3000);
+
+    });
+    document.getElementById('reloadButton').addEventListener('click', () => {
+        browser.runtime.reload();
     });
 });
