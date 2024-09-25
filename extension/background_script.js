3bcbdf68443e05f2a0dd2d4943ded43068fbb3dc
Henri Wiek <henri.wiek@hansemerkur.de>
2024-09-25 16:01:52 +0200

    feat: erster Versuch

--- extension/background_script.js
+++ extension/background_script.js
@@ -1,29 +1,61 @@
-const URL = localStorage.getItem('url');
+const REPO_URL = localStorage.getItem('url');
 const FAST_UNENDLICH = 9999;
 let ORDNER_ID = "";
 const ORDNER_NAME = localStorage.getItem('ordnerTitel');
+const FOLDER_ROLE = "folder";
+const BOOKMARK_ROLE = "bookmark";
 
-async function loadBookmarksfromGit(vorhandeneBookmarks) {
+async function synchBookmarks(vorhandeneBookmarks) {
     try {
-        const response = await fetch(URL);
+        const response = await fetch(REPO_URL);
         const jsonResponse = await response.json();
 
         for (const jsonResponseKey in jsonResponse) {
             const bookmark = jsonResponse[jsonResponseKey];
             const existingBookmark = vorhandeneBookmarks.find(e => e.title === bookmark.title);
-
-            if (existingBookmark) {
-                await browser.bookmarks.update(existingBookmark.id, {
-                    title: bookmark.title,
-                    url: bookmark.url
-                });
-            } else {
-                await browser.bookmarks.create({
-                    title: bookmark.title,
-                    url: bookmark.url,
-                    parentId: ORDNER_ID
+            if (bookmark.role === FOLDER_ROLE) {
+                console.log(bookmark)
+                browser.bookmarks.search({title: bookmark.title}).then((results) => {
+                    if (results.length > 0) {
+                        const folder = results.find(item => !item.url);
+                        if (folder) {
+                            console.log(`Folder ID: ${folder.id}, ${folder.title}`);
+                        } else {
+                            browser.bookmarks.create(
+                                {
+                                    title: bookmark.title,
+                                    parentId: ORDNER_ID
+                                }
+                            )
+                            console.log("Test")
+                        }
+                    } else {
+                        browser.bookmarks.create(
+                            {
+                                title: bookmark.title,
+                                parentId: ORDNER_ID
+                            }
+                        )
+                        console.log("Test2")
+                    }
                 });
             }
+            /*
+            if (true) {
+                if (existingBookmark) {
+                    await browser.bookmarks.update(existingBookmark.id, {
+                        title: bookmark.title,
+                        url: bookmark.url
+                    });
+                } else {
+                    await browser.bookmarks.create({
+                        title: bookmark.title,
+                        url: bookmark.url,
+                        parentId: ORDNER_ID
+                    });
+                }
+            }
+             */
         }
     } catch (error) {
         console.error('Fehler beim Laden der Bookmarks von Git:', error);
@@ -33,7 +65,7 @@ async function loadBookmarksfromGit(vorhandeneBookmarks) {
 async function loadBookmarksfromBrowser() {
     try {
         const recentBookmarks = await browser.bookmarks.getRecent(FAST_UNENDLICH);
-        await loadBookmarksfromGit(recentBookmarks);
+        await synchBookmarks(recentBookmarks);
     } catch (error) {
         console.error('Fehler beim Laden der Bookmarks aus dem Browser:', error);
     }
@@ -41,7 +73,7 @@ async function loadBookmarksfromBrowser() {
 
 async function loadBookmarkOrdner() {
     try {
-        const response = await browser.bookmarks.search({ title: ORDNER_NAME });
+        const response = await browser.bookmarks.search({title: ORDNER_NAME});
         if (response.length > 0) {
             ORDNER_ID = response[0].id;
             console.log('Ordner ID:', ORDNER_ID);
@@ -60,4 +92,34 @@ chrome.commands.onCommand.addListener(function (command) {
     }
 });
 
+function tryFolder() {
+    browser.bookmarks.create({
+        parentId: 'toolbar_____',
+        title: 'Folder Name'
+    }).then((newFolder) => {
+        console.log(`Added folder: ${newFolder}`);
+    });
+
+    browser.bookmarks.search({title: 'Folder Name'}).then((results) => {
+        if (results.length > 0) {
+            const folder = results.find(item => !item.url);
+            if (folder) {
+                console.log(`Folder ID: ${folder.id}, ${folder.title}`);
+            } else {
+                console.log('Folder not found.');
+            }
+        } else {
+            console.log('Folder not found.');
+        }
+    });
+}
+
+
+//tryFolder()
+
 loadBookmarkOrdner();
+
+/**
+ * @todo Unterordner einfuegen --> crerate ohne URL
+ * @todo JSON anpassen sodass es das es erkennbar ist ob Bookmark oder nicht und in welchen Ordner
+ */
